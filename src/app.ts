14361dbd60468b4b2d9365c175950b6d00788b8e
Darren Sim <45655404+Kalsyc@users.noreply.github.com>
2023-04-30 21:51:11 +0800

    Add in swagger UI

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { join } from "path";
 import AutoLoad, { AutoloadPluginOptions } from "@fastify/autoload";
 import { FastifyPluginAsync } from "fastify";
+import swaggerUi from "@fastify/swagger-ui";
 
 export type AppOptions = {
   // Place your custom options for app below here.
@@ -20,6 +21,28 @@ const app: FastifyPluginAsync<AppOptions> = async (fastify, opts): Promise<void>
   void fastify.register(AutoLoad, {
     dir: join(__dirname, "plugins"),
     options: opts,
+  });
+
+  void fastify.register(swaggerUi, {
+    routePrefix: "/docs",
+    uiConfig: {
+      docExpansion: "full",
+      deepLinking: false,
+    },
+    uiHooks: {
+      onRequest: function (_request: any, _reply: any, next: () => void) {
+        next();
+      },
+      preHandler: function (_request: any, _reply: any, next: () => void) {
+        next();
+      },
+    },
+    staticCSP: true,
+    transformStaticCSP: (header: any) => header,
+    transformSpecification: (swaggerObject: any, _request: any, _reply: any) => {
+      return swaggerObject;
+    },
+    transformSpecificationClone: true,
   });
 
   // This loads all plugins defined in routes
--- src/plugins/swagger.ts
+++ src/plugins/swagger.ts
@@ -2,18 +2,10 @@ import fp from "fastify-plugin";
 import swagger, { SwaggerOptions } from "@fastify/swagger";
 
 /**
- * This plugin adds swagger API documentation
+ * This plugins adds the swagger plugin
  *
  * @see https://github.com/fastify/fastify-swagger
  */
 export default fp<SwaggerOptions>(async (fastify) => {
-  fastify.register(swagger, {
-    swagger: {
-      info: {
-        title: "Swagger API",
-        description: "Yobu API",
-        version: "0.1.0",
-      },
-    },
-  });
+  fastify.register(swagger);
 });
